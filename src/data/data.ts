ba8164cd13bb7b5b015a2ce5845da328ef27f127
kyryloponomarev <kyryloponomarev0509@gmail.com>
2023-04-03 06:22:04 +0530

    clean up - starters

---
+++ src/data/data.ts
@@ -0,0 +1,106 @@
+const content = [
+  {
+    id: "article0",
+    img: "article0",
+    title:
+      "Artist Turns Meme-tastic Internet Cats Into Equally as Funny Sculptures",
+    content: `Internet cats are a never-ending source of amusement. And for some creatively minded folks, they are actually muses, too. Japanese artist Meetissai creates work inspired by funny felines and other meme-ified animals who rule our social media feeds. Meetissai transforms iconic photos into small sculptures using epoxy putty. But you don't need to be chronically online to enjoy their work. If you recognize the viral images, it adds another level of enjoyment to the creations; but if not, you can still marvel and laugh at the contorted figurines and funny expressions.
+  
+      The artist shares pictures of cats whose candid photos are snapped from certain angles to show them with extra-long legs, oddly muscular arms, and strange hats made from cut vegetables. Of course, these images are mostly optical illusions, but their awkwardness makes for surreal sculptures with incredible tiny details on their fur and faces.
+      
+      
+      Although cats are the most common creature to grace Meetissai’s portfolio, dogs, bears, and deer are also included. Whether it’s of them flattened like a pancake or two creatures combined into one, the same level of detail and humor is on these sculptures, too.
+      
+      Scroll down to see some of Meetissai’s funniest internet cat meme sculptures. If you can’t get enough, you can follow the artist on Twitter to see what they’re creating next.
+      
+      Japanese artist Meetissai creates work inspired by funny felines and other meme-ifed animals who rule our social media feeds.`,
+    author: "author1",
+  },
+  {
+    id: "article1",
+    img: "article1",
+    title: "Who is Ollie the Polite Cat and is he photoshopped?",
+    content: `Cats and memes go together like bread and butter. Since the famous “I Can Has Cheezburger?” meme arrived on our computer screens over a decade ago, cats have become one of the most popular sources of memetic and exploitable images. 
+  
+      And one of the more recent stars of the genre is Ollie the Polite Cat. But where did this meme come from, and is that popular polite expression genuine, or is it edited? 
+      
+      What is the origin of the Ollie the Polite Cat meme? 
+      The Polite Cat image can trace its origin to a post on the popular meme and image-sharing website 9Gag. This post was made by user enpu on June 7th, 2018. The post was titled: “My face when someone expects me to be excited about something that I’m not excited about.” 
+      
+      The post features the image of the smiling feline that everyone has come to love. However, the picture didn’t truly take off until it was placed into a different meme. This meme was a fake text conversation between someone and their landlord. While the landlord initially said no pets were allowed, the image of the smiling kitty won him over. Leading to the now-legendary phrase:
+      
+      “OK, I will make an exception because he looks very polite.” `,
+    author: "author2",
+  },
+  {
+    id: "article2",
+    img: "article2",
+    title: "Thurston Waffles the cat death: Fans mourn meowing meme star",
+    content: `"I can't believe I'm even writing this." Internet learns of sudden death of social media sensation Thurston Waffles in heartbreaking tweet
+  
+  
+      Thurston Waffles meowing at the camera
+    
+      With his iconic meow that saw him featured in numerous memes and his stunning white coat, Thurston Waffles was a social media sensation with a loyal and devoted following that have been left devastated by news of his sudden death.
+      
+      Hailing from North Dakota where he lived with his owners Matt and Shelli, Thurston Waffles popularity skyrocketed in 2019 after a video showing him meowing loudly in protest when Matt refused to take him for a walk went viral, accumulating more than 430,000 likes on Instagram.`,
+    author: "author2",
+  },
+  {
+    id: "article3",
+    img: "article3",
+    title: `Thanks for your kind words’: GigaChad finally responds to the memes`,
+    content: `Yes, he’s a real person. Yes, he’s noticed the “GigaChad” memes and he’s finally opened up about his internet fame
+  
+      The “GigaChad” meme emerged back in 2017. It was initially coined as an archetype on 4chan, referring to a guy who is considered to be the model of idealized hypermasculinity. The term became popular in a lot of incel (involuntary celibate) communities. The meme emerged from the @sleekntears Instagram page that was linked by an anonymous Redditor in a subreddit about bodybuilding.
+      
+      GigaChad
+      
+      The identity of the guy in the “GigaChad” meme has been a mystery for a while. Some people were even debating if the guy was a real person or if he was a CGI model someone made. The guy didn’t really have much internet presence so it was hard to tell if he was real, or whether or not the photos were just Photoshopped. He declined interviews and refused to comment on the memes.
+      
+      That is, until he finally opened up about the memes around two weeks ago. So, who is this guy, and what does he think of all the “GigaChad” memes that have made him popular? 
+      
+      “GigaChad” (also known as “Average Enjoyer”) is Russian menswear model and bodybuilder Ernest Khalimov. Aside from the “GigaChad” memes, he isn’t very well-known. Even in the “Sleek’N’Tears” project, he was exclusively known as “1969,” so it’s fair that a lot of people are surprised to hear that he’s a real person.
+      
+      Khalimov recently posted on his Instagram account berlin.1969 about the “GigaChad” memes. The Russian model and bodybuilder was very flattered and responded very humbly to the comments and the memes. He said on his post, “Thanks for your kind words. Thank you for the positive energy, don’t doubt guys I mentally return it to you.” 
+      
+      It’s hard to say if Khalimov will continue interacting with the comments and memes, but he ended his post with, “Yours faithfully, Average person.”   `,
+    author: "author2",
+  },
+  {
+    id: "article4",
+    img: "article4",
+    title: "Your kids are into Big Floppa",
+    content: `The Big Floppa meme and the fourth season of Stranger Things were the trendiest online searches of the last three months, according to a new study.
+  
+      The research, conducted by cybersecurity firm Kaspersky, has revealed the most popular kids’ interests on Google and YouTube over the three months from June to August.
+      
+      Since the launch of Season 4, Stranger Things has been wildly popular worldwide. According to statistics, the fourth season broke an all-time record as viewers watched it for 287-million hours in the week of May 23-29.  Kids also explored new games, memes and bloggers. 
+      
+      Kaspersky analysed anonymised data provided by users of parental control software Kaspersky Safe Kids from 1 June to 22 August. The data included search queries on Google and YouTube.
+      
+      Big Floppa is an internet meme based on a caracal named Gosha, also referred to as Gregory. Gregory is a less preferred name and the meme community uses the name Floppa, in reference to the caracal’s distinctive, large tufted ears. The original Big Floppa following spawned an internet subculture based on caracal memes in general.
+      
+      It was the most popular meme of this period Googled by kids, with one-fifth of all requests (20%). 
+      
+      Gaming was one of the most popular categories, with a 10% share of searches. Roblox was the most-searched game, with 33%, well ahead of Minecraft, in second place at 9%, despite its huge popularity among children. Nintendo’s recently released Pokemon Scarlet and Violet accounted for 5% of searches.`,
+    author: "author1",
+  },
+];
+
+const contentAuthor = {
+  author1: {
+    name: "Giga Floppa",
+    img: "author1",
+    content:
+      "Gives question papers to teachers.Trains the gym trainer.And more Giga Chad stuff that only Giga Chad could do",
+  },
+  author2: {
+    name: "Smudge the cat",
+    img: "author2",
+    content:
+      "Never backs down from asking questions and stating riduculous stuff. Likes being yelled at while eating food.",
+  },
+};
+
+export { content, contentAuthor };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import Header from "@/components/Header";
+import Layout from "@/components/Layout";
+import "@/styles/globals.css";
+
+import "bootstrap/dist/css/bootstrap.css";
+import type { AppProps } from "next/app";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
