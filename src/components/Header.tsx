6999572be9b89b8c583954cf5cf4fa06957459cf
kyryloponomarev <kyryloponomarev0509@gmail.com>
2023-04-05 04:47:43 +0530

    nav options based on user type

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,44 @@
-import { signOut } from "next-auth/react";
+import { IUserSession } from "@/pages/api/blog-post/handle";
+import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 import React from "react";
 
+const notLoggedInOptions = [
+  {
+    name: "Login",
+    url: "/login",
+  },
+  {
+    name: "Signup",
+    url: "/signup",
+  },
+];
+
+const readerloggedInOptions = [
+  {
+    name: "Activity Panel",
+    url: "/panel",
+  },
+];
+
+const authorAdminloggedInOptions = [
+  {
+    name: "Create Post",
+    url: "/create",
+  },
+  {
+    name: "Activity Panel",
+    url: "/panel",
+  },
+  {
+    name: "Account Options",
+    url: "/",
+  },
+];
+
 const Header = () => {
+  const { data: sessionData, status } = useSession();
+  const session: IUserSession = sessionData as IUserSession;
   return (
     <>
       <nav
@@ -31,40 +67,49 @@ const Header = () => {
                   Home
                 </Link>
               </li>
-              <li className="nav-item">
-                <Link className="nav-link" href="/login">
-                  Login
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" href="/signup">
-                  Signup
-                </Link>
-              </li>
-              <li className="nav-item">
-                <span
-                  style={{ cursor: "pointer" }}
-                  className="nav-link"
-                  onClick={() => signOut()}
-                >
-                  Logout
-                </span>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" href="/create">
-                  Create post
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" href="/">
-                  Account Options
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" href="/panel">
-                  Activity Panel
-                </Link>
-              </li>
+
+              {!["admin", "author", "reader"].includes(session?.user?.role) &&
+                notLoggedInOptions.map((currNavData) => (
+                  <li key={currNavData?.name} className="nav-item">
+                    <Link className="nav-link" href={currNavData?.url}>
+                      {currNavData?.name}
+                    </Link>
+                  </li>
+                ))}
+
+              {["reader"].includes(session?.user?.role) &&
+                readerloggedInOptions.map((currNavData) => {
+                  return (
+                    <li key={currNavData?.name} className="nav-item">
+                      <Link className="nav-link" href={currNavData?.url}>
+                        {currNavData?.name}
+                      </Link>
+                    </li>
+                  );
+                })}
+
+              {["admin", "author"].includes(session?.user?.role) &&
+                authorAdminloggedInOptions.map((currNavData) => {
+                  return (
+                    <li key={currNavData?.name} className="nav-item">
+                      <Link className="nav-link" href={currNavData?.url}>
+                        {currNavData?.name}
+                      </Link>
+                    </li>
+                  );
+                })}
+
+              {["admin", "author", "reader"].includes(session?.user?.role) && (
+                <li className="nav-item">
+                  <span
+                    style={{ cursor: "pointer" }}
+                    className="nav-link"
+                    onClick={() => signOut()}
+                  >
+                    Logout
+                  </span>
+                </li>
+              )}
             </ul>
             <form className="d-flex">
               <input
