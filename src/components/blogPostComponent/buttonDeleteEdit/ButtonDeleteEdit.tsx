73d0fb0d9dd30089c556d6062ca6eb6c82e7b165
kyryloponomarev <kyryloponomarev0509@gmail.com>
2023-04-05 19:45:55 +0530

    panel - ui handlers

--- src/components/blogPostComponent/buttonDeleteEdit/ButtonDeleteEdit.tsx
+++ src/components/blogPostComponent/buttonDeleteEdit/ButtonDeleteEdit.tsx
@@ -1,36 +1,70 @@
 import { routerPush } from "@/util/routerPush";
-import React from "react";
+import React, { useState } from "react";
 
 interface Props {
   postId: string;
 }
 
 const ButtonDeleteEdit = ({ postId }: Props) => {
+  const [confirmDel, setConfirmDel] = useState(false);
+
   const redirectEditPost = (postId: string) => {
     routerPush(`/edit/${postId}`);
   };
 
   const deleteHandler = (postId: string) => {
     console.log("postId", postId);
+    setConfirmDel((prev) => !prev);
+  };
+
+  const cancelHandler = () => {
+    setConfirmDel((prev) => !prev);
+  };
+
+  const comfirmDeleteHandler = (postId: string) => {
+    console.log("postId", postId);
+    setConfirmDel(false);
   };
 
   return (
     <>
-      <button
-        className="btn btn-outline-danger mx-2"
-        // style={{ height: "auto", width: "300px" }}
-        onClick={() => deleteHandler(postId)}
-      >
-        {`Delete`}
-      </button>
+      {!confirmDel && (
+        <>
+          <button
+            className="btn btn-outline-danger mx-2"
+            // style={{ height: "auto", width: "300px" }}
+            onClick={() => deleteHandler(postId)}
+          >
+            {`Delete`}
+          </button>
 
-      <button
-        className="btn btn-outline-warning mx-2"
-        // style={{ height: "auto", width: "300px" }}
-        onClick={() => redirectEditPost(postId)}
-      >
-        {`Edit`}
-      </button>
+          <button
+            className="btn btn-outline-warning mx-2"
+            // style={{ height: "auto", width: "300px" }}
+            onClick={() => redirectEditPost(postId)}
+          >
+            {`Edit`}
+          </button>
+        </>
+      )}
+      {confirmDel && (
+        <>
+          <button
+            className="btn btn-outline-secondary p-1 mx-2"
+            // style={{ height: "auto", width: "300px" }}
+            onClick={() => cancelHandler()}
+          >
+            {`Back`}
+          </button>
+          <button
+            className="btn btn-outline-danger p-1 mx-2"
+            // style={{ height: "auto", width: "300px" }}
+            onClick={() => comfirmDeleteHandler(postId)}
+          >
+            {`Confirm`}
+          </button>
+        </>
+      )}
     </>
   );
 };
