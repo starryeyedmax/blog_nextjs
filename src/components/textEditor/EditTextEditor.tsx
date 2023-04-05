dfbd75f173d36b1fa93c49f34c952258a759be93
kyryloponomarev <kyryloponomarev0509@gmail.com>
2023-04-05 20:22:44 +0530

    panel - edit starters

--- src/components/textEditor/EditTextEditor.tsx
+++ src/components/textEditor/EditTextEditor.tsx
@@ -4,14 +4,29 @@ import QuillComponent from "./quill/QuillComponent";
 import { useTextEditorContext } from "./hooks/useTextEditorContext";
 import { useEffect } from "react";
 
-const EditTextEditor = () => {
-  const { title, htmlData } = useTextEditorContext();
-
+const EditTextEditor = ({ parsedFullBlogPostData }: any) => {
+  const { title, htmlData, setDelta, setTitle, setDescription } =
+    useTextEditorContext();
+  console.log(parsedFullBlogPostData, "editor");
   console.log(title, "using the usecontext");
   useEffect(() => {
     if (!htmlData) return;
-    console.log(htmlData);
-  }, [htmlData]);
+    // console.log(htmlData);
+    console.log(htmlData, "rerender");
+    console.log(parsedFullBlogPostData, "editor");
+  }, []);
+
+  useEffect(() => {
+    if (
+      typeof parsedFullBlogPostData?.bodyHTML !== "undefined" ||
+      typeof parsedFullBlogPostData?.title !== "undefined" ||
+      typeof parsedFullBlogPostData?.description !== "undefined"
+    ) {
+      setDelta(parsedFullBlogPostData?.bodyHTML);
+      setTitle(parsedFullBlogPostData?.title);
+      setDescription(parsedFullBlogPostData?.description);
+    }
+  }, [parsedFullBlogPostData]);
 
   return (
     <div className="login-form-bg  pt-5 pb-5 pe-md-5 px-md-5 mb-5">
--- src/pages/edit/[id]/index.tsx
+++ src/pages/edit/[id]/index.tsx
@@ -1,12 +1,30 @@
-import TextEditor from "@/components/textEditor/TextEditor";
+import EditTextEditor from "@/components/textEditor/EditTextEditor";
+
+import TextEditorContextProvider from "@/components/textEditor/hooks/textEditorContext";
+import { serverSideBlogSinglePost } from "@/getServerSideCalls/getServerSideCalls-PostPage-Comments";
 import React from "react";
 
-const EditPost = () => {
+const EditPost = ({ fullBlogPostData }: any) => {
+  const parsedFullBlogPostData = JSON.parse(fullBlogPostData)[0];
+  console.log(parsedFullBlogPostData, "parsedfullBlogPostData");
+
   return (
     <div>
-      <TextEditor />
+      <TextEditorContextProvider>
+        <EditTextEditor parsedFullBlogPostData={parsedFullBlogPostData} />
+      </TextEditorContextProvider>
     </div>
   );
 };
 
 export default EditPost;
+
+// This gets called on every request
+export async function getServerSideProps(context: any) {
+  console.log("oofffffffff");
+
+  const { fullBlogPostData } = await serverSideBlogSinglePost(context);
+
+  // Pass data to the page via props
+  return { props: { fullBlogPostData } };
+}
